b73f8e65a14c490867c8af4d159712b91e1dfc33
sadid56 <sadidhasan56@gmail.com>
2025-10-27 02:29:41 +0600

    Change project horizental scroll framer motion to gsap

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -2,32 +2,18 @@ export const dynamic = "force-static";
 
 import { LinkPreview } from "@/components/ui/LinkPreview";
 import Icon from "@/components/global/Icon";
-import {
-  IconBrandDiscord,
-  IconBrandFacebook,
-  IconBrandGithub,
-  IconBrandLinkedin,
-} from "@tabler/icons-react";
+import { IconBrandDiscord, IconBrandFacebook, IconBrandGithub, IconBrandLinkedin } from "@tabler/icons-react";
 import CenterVideo from "@/components/sections/Hero/HeroCenterVideo";
 import styles from "@/styles/helo.module.css";
 import { HeroParallaxItems } from "./HerroParallaxItems";
 import { Suspense } from "react";
 
-// ssg render
-export async function getStaticProps() {
-  return {
-    props: {},
-  };
-}
 const Hero = () => {
   const SECTION_HEIGHT = 1600;
 
   return (
-    <header id="home">
-      <div
-        style={{ height: `calc(${SECTION_HEIGHT}px + 100vh)` }}
-        className="relative w-full"
-      >
+    <header id='home'>
+      <div style={{ height: `calc(${SECTION_HEIGHT}px + 100vh)` }} className='relative w-full'>
         <Suspense>
           <CenterVideo />
         </Suspense>
@@ -35,25 +21,25 @@ const Hero = () => {
           <HeroParallaxItems />
         </Suspense>
         {/* hero bottom  overlay */}
-        <div className="absolute bottom-0 left-0 right-0 h-96 bg-gradient-to-b from-zinc-950/0 to-mainBgColor" />
+        <div className='absolute bottom-0 left-0 right-0 h-96 bg-gradient-to-b from-zinc-950/0 to-mainBgColor' />
         {/* right side scroll icon */}
-        <div className="fixed bottom-5 right-3 md:right-5 flex-col items-center gap-10 flex">
-          <div className="flex flex-col gap-3 items-center ">
-            <LinkPreview url="https://github.com/sadid56/">
+        <div className='fixed bottom-5 right-3 md:right-5 flex-col items-center gap-10 flex'>
+          <div className='flex flex-col gap-3 items-center '>
+            <LinkPreview url='https://github.com/sadid56/'>
               <Icon Icon={IconBrandGithub} widht={2} />
             </LinkPreview>
-            <LinkPreview url="https://www.facebook.com/sadidhasanx">
+            <LinkPreview url='https://www.facebook.com/sadidhasanx'>
               <Icon Icon={IconBrandFacebook} widht={2} />
             </LinkPreview>
-            <LinkPreview url="https://discord.com/users/1151169623149002752/">
+            <LinkPreview url='https://discord.com/users/1151169623149002752/'>
               <Icon Icon={IconBrandDiscord} widht={2} />
             </LinkPreview>
-            <LinkPreview url="https://www.linkedin.com/in/mr-sadid/">
+            <LinkPreview url='https://www.linkedin.com/in/mr-sadid/'>
               <Icon Icon={IconBrandLinkedin} widht={2} />
             </LinkPreview>
           </div>
-          <div className="relative">
-            <p className="text-slate-300 text-sm absolute rotate-90 -right-1 top-8 font-poppins">
+          <div className='relative'>
+            <p className='text-slate-300 text-sm absolute rotate-90 -right-1 top-8 font-poppins'>
               <small>Scroll</small>
             </p>
             <div className={styles?.arrow}></div>
--- src/components/sections/Project/Projects.tsx
+++ src/components/sections/Project/Projects.tsx
@@ -1,134 +1,133 @@
 "use client";
-import { useRef, useState } from "react";
+import { useRef, useEffect, useState } from "react";
 import SectionTitle from "@/components/global/SectionTitle";
 import { LinkPreview } from "@/components/ui/LinkPreview";
-import { IconBrandGithub, IconServer, IconWorld } from "@tabler/icons-react";
-import { useScroll, useTransform, motion } from "motion/react";
-import { Project } from "@/types/ProjectTypes";
+import { IconBrandGithub, IconPlayerPlay, IconServer, IconWorld } from "@tabler/icons-react";
 import BoxReveal from "@/components/ui/BoxReveal";
-import { VideoDialog } from "@/components/ui/VideoDialog";
 import Link from "next/link";
 import { UrlObject } from "url";
 import projects from "@/data/projects";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Image from "next/image";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Projects = () => {
-  const targetRef = useRef(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const cardsRef = useRef<HTMLDivElement[]>([]);
+  cardsRef.current = [];
   const [isVideoOpen, setIsVideoOpen] = useState(false);
-  const { scrollYProgress } = useScroll({
-    target: targetRef,
-  });
 
-  // horizental scroll progress
-  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-90%"]);
-  let transform = isVideoOpen ? undefined : { x };
+  const addToRefs = (el: HTMLDivElement) => {
+    if (el && !cardsRef.current.includes(el)) cardsRef.current.push(el);
+  };
 
-  // main content
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    const totalCards = cardsRef.current.length;
+
+    gsap.to(cardsRef.current, {
+      xPercent: -100 * (totalCards - 1),
+      ease: "none",
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: "top top",
+        end: () => `+=2000`,
+        pin: true,
+        scrub: 1,
+        snap: 1 / (totalCards - 0.5), // snap to each card
+        anticipatePin: 1,
+        invalidateOnRefresh: true,
+      },
+    });
+  }, []);
+
   return (
     <div id='projects' className='max-w-[1300px] mx-auto relative'>
       <SectionTitle text='Projects_' color='My' />
-      <div ref={targetRef} className='relative h-[300vh]'>
-        <div className='sticky top-0 flex h-screen items-center overflow-hidden'>
-          <motion.div
-            style={transform}
-            initial={{ opacity: 0, y: 100 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            // transition={{ duration: 0.8, ease: "easeOut" }}
-            viewport={{ once: false }}
-            className='flex gap-8 snap-mandatory'
-            transition={{ type: "spring", stiffness: 80, damping: 15 }}
-          >
-            {projects.map((card: Project, idx) => {
-              return <Card project={card} key={idx + 1} isVideoOpen={isVideoOpen} setIsVideoOpen={setIsVideoOpen} />;
-            })}
-          </motion.div>
+      <div ref={containerRef} className='relative h-screen overflow-hidden'>
+        <div className='flex items-center h-full w-full gap-10'>
+          {projects.map((card, idx) => (
+            <div ref={addToRefs} key={idx} className='flex-shrink-0 w-full md:w-[900px] mx-auto h-[420px] md:h-[400px] snap-center'>
+              <Card project={card} isVideoOpen={isVideoOpen} setIsVideoOpen={setIsVideoOpen} />
+            </div>
+          ))}
         </div>
       </div>
-      <div className='w-48 h-48 bg-[#03e8f4aa] blur-[180px] absolute -top-24 -left-32'></div>
     </div>
   );
 };
 
-// project card
-
-const Card = ({ project, setIsVideoOpen, isVideoOpen }: any) => {
+const Card = ({ project }: any) => {
   return (
-    <motion.div
-    // whileHover={{ scale: 1.02 }}
-    // transition={{ type: "spring", stiffness: 150, damping: 15 }}
-    // className="relative"
-    >
-      {/* Outer shiny border */}
-      <div className=' h-[420px] md:h-[400px] w-[90vw] lg:w-[900px] snap-center rounded-2xl p-[2px] bg-gradient-to-r from-[#03e8f4] via-[#6a5acd] to-[#03e8f4] animate-border'>
-        {/* Inner card */}
-        <div className='flex flex-col md:flex-row gap-4 md:gap-7 rounded-2xl h-full bg-black/90 border border-white/10 shadow-lg hover:shadow-[#03e8f480]/40 transition-all duration-500 p-4'>
-          {/* Video */}
-          <div className='w-full md:w-[40%] rounded-xl overflow-hidden relative'>
-            <VideoDialog
-              className='w-full h-full'
-              animationStyle='from-center'
-              setIsVideoOpen={setIsVideoOpen}
-              isVideoOpen={isVideoOpen}
-              videoSrc='https://www.youtube.com/embed/NtfbWkxJTHw?si=WyeCuKiwW0JwxexI'
-              thumbnailSrc='https://i.ytimg.com/vi/NtfbWkxJTHw/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDqq9DkE96fZqe3np_pvXTTddliJw'
-              thumbnailAlt='Hero Video'
-            />
-          </div>
+    <div className='rounded-2xl p-[2px] bg-gradient-to-r from-[#03e8f4] via-[#6a5acd] to-[#03e8f4] animate-border h-full w-full'>
+      <div className='flex flex-col md:flex-row gap-4 md:gap-7 rounded-2xl h-full bg-black/90 border border-white/10 shadow-lg hover:shadow-[#03e8f480]/40 transition-all duration-500 p-4'>
+        <div className='w-full md:w-[40%] rounded-xl overflow-hidden relative group'>
+          <Image
+            src={
+              "https://i.ytimg.com/vi/NtfbWkxJTHw/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDqq9DkE96fZqe3np_pvXTTddliJw"
+            }
+            alt={project?.project_name}
+            width={500}
+            height={500}
+            className='w-full transition-all duration-200 group-hover:brightness-[0.8] ease-out rounded-md shadow-lg h-36 md:h-full object-cover'
+          />
 
-          {/* Text content */}
-          <div className='md:w-[60%] z-20'>
-            <h3 className='text-xl md:text-3xl font-semibold text-white bg-gradient-to-r from-[#03e8f4] to-[#6a5acd] bg-clip-text font-montserrat py-1 w-full'>
-              <BoxReveal duration={0.8}>
-                <span>{project?.project_name}</span>
-              </BoxReveal>
-            </h3>
-
-            <p className='text-sm md:text-[16px] text-slate-300 leading-6 font-poppins hidden md:block mt-3'>{project?.description}</p>
-            <p className='text-sm md:text-[16px] truncate text-slate-400 leading-6 font-poppins md:hidden mt-2'>{project?.description}</p>
-
-            {/* technologies */}
-            <h5 className='text-[12px] mt-3 md:text-sm text-slate-300 font-medium font-poppins'>
-              Technologies: <span className='text-slate-400'>{project?.technology?.join(", ")}</span>
-            </h5>
-
-            {/* Contributors */}
-            {project?.contributors && (
-              <div className='mt-2 hidden md:block'>
-                <p className='font-medium text-sm font-montserrat text-slate-300'>Contributors:</p>
-                <div className='space-x-2'>
-                  {project?.contributors?.map((item: { fb: string | UrlObject; name: string }, i: number) => (
-                    <Link
-                      target='_blank'
-                      key={i + 1}
-                      href={item.fb}
-                      className='text-[#03e8f4] text-[13px] font-medium font-poppins hover:underline'
-                    >{`@${item?.name}`}</Link>
-                  ))}
-                </div>
-              </div>
-            )}
-
-            {/* Links */}
-            <div className='flex gap-5 mt-3'>
-              <LinkPreview url={project?.live_link}>
-                <div className='hover:bg-[#03e8f4] shadow shadow-[#03e8f4] transition-all duration-500 border border-[#03e8f4] flex items-center justify-center w-10 h-10 rounded-full text-white'>
-                  <IconWorld />
-                </div>
-              </LinkPreview>
-              <LinkPreview url={project?.client_github_link}>
-                <div className='hover:bg-[#6a5acd] shadow shadow-[#6a5acd] transition-all duration-500 border border-[#6a5acd] flex items-center justify-center w-10 h-10 rounded-full text-white'>
-                  <IconBrandGithub />
-                </div>
-              </LinkPreview>
-              <LinkPreview url={project?.server_github_link}>
-                <div className='hover:bg-[#00ffaa] shadow shadow-[#00ffaa] transition-all duration-500 border border-[#00ffaa] flex items-center justify-center w-10 h-10 rounded-full text-white'>
-                  <IconServer />
-                </div>
-              </LinkPreview>
+          {/* play icon  */}
+          <div className='absolute inset-0 flex items-center justify-center cursor-pointer'>
+            <div className='bg-gradient-to-br from-[#03e8f4]/40 via-[#6a5acd]/40 to-[#03e8f4]/40 backdrop-blur-md rounded-full w-14 h-14 flex items-center justify-center transform scale-90 group-hover:scale-105 transition-transform duration-300'>
+              <IconPlayerPlay className='text-white size-8' />
             </div>
           </div>
         </div>
+        <div className='md:w-[60%] z-20'>
+          <h3 className='text-xl md:text-3xl font-semibold text-white bg-gradient-to-r from-[#03e8f4] to-[#6a5acd] bg-clip-text font-montserrat py-1 w-full'>
+            <BoxReveal duration={0.8}>
+              <span>{project?.project_name}</span>
+            </BoxReveal>
+          </h3>
+          <p className='text-sm md:text-[16px] text-slate-300 leading-6 font-poppins hidden md:block mt-3'>{project?.description}</p>
+          <p className='text-sm md:text-[16px] truncate text-slate-400 leading-6 font-poppins md:hidden mt-2'>{project?.description}</p>
+          <h5 className='text-[12px] mt-3 md:text-sm text-slate-300 font-medium font-poppins'>
+            Technologies: <span className='text-slate-400'>{project?.technology?.join(", ")}</span>
+          </h5>
+          {project?.contributors && (
+            <div className='mt-2 hidden md:block'>
+              <p className='font-medium text-sm font-montserrat text-slate-300'>Contributors:</p>
+              <div className='space-x-2'>
+                {project?.contributors?.map((item: { fb: string | UrlObject; name: string }, i: number) => (
+                  <Link
+                    target='_blank'
+                    key={i + 1}
+                    href={item.fb}
+                    className='text-[#03e8f4] text-[13px] font-medium font-poppins hover:underline'
+                  >{`@${item?.name}`}</Link>
+                ))}
+              </div>
+            </div>
+          )}
+          <div className='flex gap-5 mt-3'>
+            <LinkPreview url={project?.live_link}>
+              <div className='hover:bg-[#03e8f4] shadow shadow-[#03e8f4] transition-all duration-500 border border-[#03e8f4] flex items-center justify-center w-10 h-10 rounded-full text-white'>
+                <IconWorld />
+              </div>
+            </LinkPreview>
+            <LinkPreview url={project?.client_github_link}>
+              <div className='hover:bg-[#6a5acd] shadow shadow-[#6a5acd] transition-all duration-500 border border-[#6a5acd] flex items-center justify-center w-10 h-10 rounded-full text-white'>
+                <IconBrandGithub />
+              </div>
+            </LinkPreview>
+            <LinkPreview url={project?.server_github_link}>
+              <div className='hover:bg-[#00ffaa] shadow shadow-[#00ffaa] transition-all duration-500 border border-[#00ffaa] flex items-center justify-center w-10 h-10 rounded-full text-white'>
+                <IconServer />
+              </div>
+            </LinkPreview>
+          </div>
+        </div>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
